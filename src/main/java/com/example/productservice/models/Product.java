4106595d12edfb322289443943e27c50f99bfe04
amir <amir@wholphintech.com>
2025-05-02 15:56:43 +0530

    Initial commit

---
+++ src/main/java/com/example/productservice/models/Product.java
@@ -0,0 +1,10 @@
+package com.example.productservice.models;
+
+public class Product {
+    private long id;
+    private String name;
+    private String description;
+    private double price;
+    private String imageUrl;
+    private Category category;
+}
